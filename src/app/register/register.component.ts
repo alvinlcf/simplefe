f50d5b12aa1629a82dce781a7aeae9f6f1502580
Alvinlcf <alvinleongcheefoo@gmail.com>
2022-01-05 15:05:20 +0800

    fix the report load issue

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -23,11 +23,7 @@ export class RegisterComponent implements OnInit {
         private trackerService: TrackerService,
         private keycloakService: KeycloakService,
         private alertService: AlertService
-
-    ) {
-
-
-    }
+    ) { }
 
     ngOnInit() {
         this.loadListingContent();
@@ -40,17 +36,16 @@ export class RegisterComponent implements OnInit {
         const lTableauTrusted = "trusted/";
         // const lTableauView = "Tableau12june2020/Dashboard6";
         const lTableauView = "TornadoReport/Story1";
-        
-        redeemTrustedPng(this.alertService, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView);
 
-
+        redeemTrustedPng(this.alertService, this.trackerService, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView);
     }
+
     redirectToMain() {
         this.router.navigate(['/main']);
     }
 
     logout() {
-        this.keycloakService.logout("http://"+environment.vmip+":4201/");
+        this.keycloakService.logout("http://" + environment.vmip + ":4201/");
     }
 
     loadListingContent() {
@@ -187,18 +182,18 @@ export class RegisterComponent implements OnInit {
 
 
 
-function redeemTrustedPng(alertService, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView) {
+function redeemTrustedPng(alertService, trackerService, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView) {
     // Now load the image, but not actual place in the visible part of the DOM
     var redemptionImg = new Image();
     redemptionImg.onload = function () {
         console.log("Trusted ticket redeemed!")
         alertService.clear();
-        trustedImageLoadResponse(alertService, true, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView);
+        trustedImageLoadResponse(alertService, trackerService, true, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView);
     }
     redemptionImg.onerror = function () {
         console.log("Trusted ticket image retrieval failed");
         alertService.error("This site uses 'Cookies'. Please enable browser 'Cookies' and refresh the page.");
-        trustedImageLoadResponse(alertService, false, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView);
+        trustedImageLoadResponse(alertService, trackerService, false, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView);
     }
 
     // Actually load the image here
@@ -206,48 +201,60 @@ function redeemTrustedPng(alertService, lUsername, lTableauSite, lTableauHostUrl
     //"https://test-tableau.5flow.net/t/Tornado/views/Tableau12june2020/Dashboard6.png";
 }
 
-function trustedImageLoadResponse(alertService, response, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView) {
+function trustedImageLoadResponse(alertService, trackerService, response, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView) {
     console.log("trustedImageLoadResponse===" + response)
 
     if (!response) {
-        getTrustedTicket(alertService, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView);
+        getTrustedTicket(alertService, trackerService, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView);
     }
 }
 
-function getTrustedTicket(alertService, aUsername, aTableauSite, aTableauHostUrl, aTableauTrusted, aTableauView) {
+function getTrustedTicket(alertService, trackerService, lUsername, lTableauSite, lTableauHostUrl, lTableauTrusted, lTableauView) {
     var lResult;
-    var lParams = {
-        'target_site': aTableauSite,
-        'username': aUsername
+    // final acceptable JSON format for BE to process
+    let tableauDTO: {
+        username: string,
+        target_site: string
+    } = {
+        username: lUsername,
+        target_site: lTableauSite
     };
 
-    $.ajax({
-        type: "POST",
-        url: "/reports/getTableauTicket",
-        data: JSON.stringify(lParams),
-        contentType: "application/json",
-        success: function (result) {
-            console.log("result")
-            lResult = result.dataList[0].trustedTicket;
-            console.log(lResult)
-        },
-        error: function (error) {
-            console.log("Tableau Trusted Ticket ERROR:");
-            console.log(error);
-            return false;
-        },
-        complete: function () {
-            console.log("scompleted");
-            console.log("aticket here=");
-            console.log(lResult);
-            alertService.clear();
+    trackerService.getTableauTicket(tableauDTO).pipe()
+        .subscribe(
+            (res: TableauDTO[]) => {
+                console.log(res);
+                if (res.length > 0) {
+                    console.log("result")
+                    lResult = res[0].trustedTicket;
+                    console.log(lResult)
+                }
+            },
+            err => {
+                if (err.status == "401" && err.statusText == "Unauthorized") {
+                    console.log("401")
+                    this.message = err.statusText;
+                }
+                else if (err.status == "500" && err.statusText == "Internal Server Error") {
+                    console.log("500")
+                    this.message = err.statusText;
+                }
+                else {
+                    console.log("Tableau Trusted Ticket ERROR:");
+                    console.log(err)
+                    this.message = err.statusText + " - " + err.error.message;
+                }
+            },
+            () => {
+                console.log("scompleted");
+                console.log("aticket here=");
+                console.log(lResult);
+                alertService.clear();
 
-            const vizUrl = aTableauHostUrl + aTableauTrusted + lResult + "/t/" + aTableauSite + "/views/" + aTableauView;
+                const vizUrl = lTableauHostUrl + lTableauTrusted + lResult + "/t/" + lTableauSite + "/views/" + lTableauView;
 
-            if (lResult != undefined) {
-                $('#tableauIframe').attr('src', vizUrl);
-            }
-        }
-    });
-
+                if (lResult != undefined) {
+                    $('#tableauIframe').attr('src', vizUrl);
+                }
+            });
 };
